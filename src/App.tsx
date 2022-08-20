dbd79c8525fea45a48698b1a1fca028281135eff
ak1103dev <ak.apichan1103@gmail.com>
2022-08-20 21:12:57 +0700

    :construction: Entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { remult } from "./common";
+import { Task } from "./shared/Task";
+
+const taskRepo = remult.repo(Task);
+
+async function fetchTasks() {
+  return taskRepo.find();
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    fetchTasks().then(setTasks);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <input type="checkbox" checked={task.completed} />
+          {task.title}
+        </div>
+      ))}
     </div>
   );
 }
