698ac2573050d7497e3eb9022906a6df8f130184
ak1103dev <ak.apichan1103@gmail.com>
2022-08-20 21:31:40 +0700

    :construction: edit task

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,27 @@ function App() {
         checked={hideCompleted}
         onChange={e => setHideCompleted(e.target.checked)} /> Hide Completed
       <hr />
-      {tasks.map(task => (
-        <div key={task.id}>
-          <input type="checkbox" checked={task.completed} />
-          {task.title}
-        </div>
-      ))}
+      {tasks.map(task => {
+        const handleChange = (values: Partial<Task>) => {
+          setTasks(tasks.map(t => t === task ? { ...task, ...values } : t));
+        };
+
+        const saveTask = () => {
+          taskRepo.save(task);
+       };
+
+        return (
+          <div key={task.id}>
+            <input type="checkbox"
+              checked={task.completed}
+              onChange={e => handleChange({ completed: e.target.checked })} />
+            <input
+              value={task.title}
+              onChange={e => handleChange({ title: e.target.value })} />
+            <button onClick={saveTask}>Save</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
