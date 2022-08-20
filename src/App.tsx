9b9d7db3d590b7b3ff120f3f7612daa39ff67f49
ak1103dev <ak.apichan1103@gmail.com>
2022-08-20 21:21:15 +0700

    :construction: Paging, Sorting and Filtering

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import { Task } from "./shared/Task";
 
 const taskRepo = remult.repo(Task);
 
-async function fetchTasks() {
-  return taskRepo.find();
+async function fetchTasks(hideCompleted: boolean) {
+  return taskRepo.find({
+     limit: 20,
+     orderBy: { completed: "asc" },
+     where: { completed: hideCompleted ? false : undefined }
+  });
 }
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [hideCompleted, setHideCompleted] = useState(false);
 
   useEffect(() => {
-    fetchTasks().then(setTasks);
-  }, []);
+    fetchTasks(hideCompleted).then(setTasks);
+ }, [hideCompleted]);
 
   return (
     <div>
+       <input
+        type="checkbox"
+        checked={hideCompleted}
+        onChange={e => setHideCompleted(e.target.checked)} /> Hide Completed
+      <hr />
       {tasks.map(task => (
         <div key={task.id}>
           <input type="checkbox" checked={task.completed} />
